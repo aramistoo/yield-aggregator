ebb1f043066a229a7a6ccd518cc0e94a442ccdf8
aramistoo <floorads@gmail.com>
2021-12-31 08:54:56 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ class Navbar extends Component {
             <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
                 <a
                     className="navbar-brand col-sm-3 col-md-2 mr-0 mx-3"
-                    href="http://www.dappuniversity.com/bootcamp"
+                    href="http://www.ibak.com"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Dapp University
+                    Living Israel Crypto Space
                 </a>
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
